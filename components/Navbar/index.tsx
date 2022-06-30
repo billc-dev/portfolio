dad626ff3981ef673573f13829ca12253e36eb80
billchengdev <chengbill0418@gmail.com>
2022-06-30 17:45:54 +0800

    feat: underline link if hash matches

--- components/Navbar/index.tsx
+++ components/Navbar/index.tsx
@@ -1,7 +1,10 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Navbar = () => {
+  const { asPath } = useRouter();
+  const hash = asPath.split("#")[1];
   return (
     <nav className="fixed top-0 w-full bg-zinc-900 z-50 py-2">
       <div className="max-w-6xl mx-auto px-4 md:px-8 flex justify-between items-center">
@@ -10,10 +13,10 @@ const Navbar = () => {
         </Link>
         <div className="">
           <Link href="#work">
-            <a className="ml-8">Work</a>
+            <a className={`ml-8 ${hash === "work" && "underline"}`}>Work</a>
           </Link>
           <Link href="#about">
-            <a className="ml-8">About</a>
+            <a className={`ml-8 ${hash === "about" && "underline"}`}>About</a>
           </Link>
           <Link href="/BillCheng_CV.pdf" download>
             <a className="ml-8" target="_blank">
