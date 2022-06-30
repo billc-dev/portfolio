8ab5e3c0d2fa0bc0207c2c4037f4efa948dbd379
billchengdev <chengbill0418@gmail.com>
2022-06-30 23:15:00 +0800

    feat: add title and favicon

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from "next";
+import Head from "next/head";
 import Hero from "../components/Hero";
 import Navbar from "../components/Navbar";
 import RecentWork from "../components/Recent Work";
@@ -6,6 +7,29 @@ import RecentWork from "../components/Recent Work";
 const Home: NextPage = () => {
   return (
     <>
+      <Head>
+        <title>Bill Cheng</title>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+        <link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+      </Head>
       <Navbar />
       <div className="mx-auto mt-12 max-w-6xl p-4 pt-2 md:p-8">
         <Hero />
